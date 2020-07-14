3b298a1b9d30e469605d68ebec4f057bcad20bf6
jeremiahidoko <jeremiahidoko@gmail.com>
2020-07-14 17:43:22 +0100

    updated models.py

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from sqlalchemy import Column, String, Integer, create_engine, Date
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "casting"
-database_path = "postgresql://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost:5432', database_name)
+# database_name = "casting"
+# database_path = "postgresql://{}:{}@{}/{}".format('postgres', 'postgres', 'localhost:5432', database_name)
 # database_path = "postgres://iogkdbxvphllfy:fe8d4ac98d57169e54886aea6b120f0ca226f903713f40f96e2c61ec8a6ac209@ec2-54-146-4-66.compute-1.amazonaws.com:5432/da3btsuh5nciev"
 
 db = SQLAlchemy()
@@ -15,19 +15,22 @@ setup_db(app)
 '''
 
 
-def setup_db(app, database_path=database_path):
-# def setup_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+# def setup_db(app, database_path=database_path):
+def setup_db(app):
+    # app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
     db.create_all()
 
-# TO DROP AND CREATE DATABASE 
+# TO DROP AND CREATE DATABASE
+
+
 def db_drop_and_create_all():
   db.drop_all()
   db.create_all()
+
 
 '''
 Movies
