bf508f9d5762493e98c6100fbc4dffdb764c50a8
jeremiahidoko <jeremiahidoko@gmail.com>
2020-07-14 17:43:59 +0100

    created migration files

---
+++ setup.sh
@@ -0,0 +1,4 @@
+export AUTH0_DOMAIN='joi.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='Casting'
+export DATABASE_URL=postgres://jjazntkywoofvp:3078103b87a442125ef228f57dccb8b4370946d4061b5bca71311e0f3d6d5009@ec2-52-70-15-120.compute-1.amazonaws.com:5432/d4gk4io76sphs0
