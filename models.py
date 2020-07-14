01b17f169822a924f90641e95d0b339b154635a1
jeremiahidoko <jeremiahidoko@gmail.com>
2020-07-14 17:58:42 +0100

    updated models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import Column, String, Integer, create_engine, Date
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
 database_name = "casting"
@@ -20,6 +21,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    migrate = Migrate(app, db)
     db.app = app
     db.init_app(app)
     db.create_all()
